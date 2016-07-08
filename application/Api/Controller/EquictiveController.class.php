d05b51d42d8561968bacc4109217231bb0e2a7c1
zharty <zharty@163.com>
2016-07-08 11:11:04 +0800

    7-8-7

--- application/Api/Controller/EquictiveController.class.php
+++ application/Api/Controller/EquictiveController.class.php
@@ -15,20 +15,19 @@ class EquictiveController extends Controller {
         $deviceid=I('cdkey');
         if(!$deviceid){
         	echo 0;
-        	return;
+        	exit();
         }
         $device=D('equictive')->where("cdkey='%s'",array($deviceid))->find();  
         if($device){
         	echo 1;
-        	return ;
+        	exit();
         }else{
         	$data['cdkey']=$deviceid;
         	$data['status']=1;
         	$data['version']=I('vn');
         	$result=D('equictive')->add($data);
         	echo 2;
-        	return;
-        	
+        	exit();
         }
 
 
