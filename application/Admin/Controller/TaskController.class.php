fa776d473b5825564387bd6e6a682520e6312311
liu <1227642768@qq.com>
2016-07-15 17:29:50 +0800

    任务管理修改

--- application/Admin/Controller/TaskController.class.php
+++ application/Admin/Controller/TaskController.class.php
@@ -85,7 +85,19 @@ class TaskController extends AdminbaseController{
 		$parame['abcuser']=$_POST['abcuser'];
 		$parame['abcpwd']=$_POST['abcpwd'];
 		$parame['joinbusi']=$_POST['joinbusi'];
-
+		$parame_mustts = $this->coldata('mustt');
+		$parame_mingle = $this->coldata('mingle');
+		foreach($parame_mustts as $k=>$v){
+			if(isset($parame_mustts[$k]) && $v !='' && $v != 0){
+				$parame[$k] = $v;
+			}
+		}
+		foreach($parame_mingle as $k=>$v){
+			if(isset($parame_mingle[$k]) && $v !='' && $v != 0){
+				$parame[$k] = $v;
+			}
+		}
+		
 		$data['parame']=serialize($parame);
 
 		$runcode=D('runcode');			
@@ -108,6 +120,8 @@ class TaskController extends AdminbaseController{
 			$data['taskname']=I('taskname');			
 			$data['alterip']=I('alterip');
 			$data['addtime']=time();
+			
+			
 			$data['mustt']=serialize($_POST['mustt']);
 			$data['mingle']=serialize($_POST['mingle']);
 			$data['weixicut']=$_POST['weixicut'];
@@ -122,7 +136,19 @@ class TaskController extends AdminbaseController{
 			$parame['abcuser']=$_POST['abcuser'];
 			$parame['abcpwd']=$_POST['abcpwd'];
 			$parame['joinbusi']=$_POST['joinbusi'];
-
+			$parame_mustts = $this->coldata('mustt');
+			$parame_mingle = $this->coldata('mingle');
+			foreach($parame_mustts as $k=>$v){
+				if(isset($parame_mustts[$k]) && $v !='' && $v != 0){
+					$parame[$k] = $v;
+				}
+			}
+			foreach($parame_mingle as $k=>$v){
+				if(isset($parame_mingle[$k]) && $v !='' && $v != 0){
+					$parame[$k] = $v;
+				}
+			}
+			
 			//$parame['nickename']=$_POST['nickename'];
 
 			$data['parame']=serialize($parame);
@@ -151,6 +177,22 @@ class TaskController extends AdminbaseController{
         $this->assign('instruct',$list);		
         $this->display();
 	}
+	
+	protected function coldata($col='mustt'){
+		$mustt = I($col);
+		foreach($mustt as $k=>$v){
+			$mustts[][] = $v;
+		}
+		foreach($mustts as $k=>$v){
+			$column_name = I($col.'_colname_'.$v[0]);
+			foreach($column_name as $k1=>$v1){
+				$mustts[$col.'_'.$v[0].'_'.$v1] = I($col.'_'.$v1.'_'.$v[0]);
+			}
+			unset($mustts[$k]);
+		}
+		return $mustts;
+	}
+	
 	public function mobile(){
 		$this->display();
 	}
