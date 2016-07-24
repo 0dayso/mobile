29f1627f6d34fef46981ae48b8706d81a77cb926
zharty <zharty@163.com>
2016-07-24 17:42:53 +0800

    7-24

--- application/Admin/Controller/MobileaddController.class.php
+++ application/Admin/Controller/MobileaddController.class.php
@@ -12,6 +12,7 @@ class MobileaddController extends AdminbaseController{
 	public function index(){
 		$GLOBALS['z']=$GLOBALS['z']+1;
 		M()->startTrans();
+		$count=M('mobile')->where('status=0 and type=2 and isshow=0')->count();
         $isallsave=true;  
 		$data=M('mobile')->where('status=0 and type=2 and isshow=0')->limit(5)->lock(true)->getfield('id,mobile',true);
 		foreach ($data as $key => $value) {
