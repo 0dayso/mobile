b83e8b3c03dd493a38e4f8921b96b5be190ef337
liu <1227642768@qq.com>
2016-08-05 10:10:15 +0800

    设备管理修改

--- application/Admin/Controller/EquipmentController.class.php
+++ application/Admin/Controller/EquipmentController.class.php
@@ -27,6 +27,7 @@ class EquipmentController extends AdminbaseController {
     public function mobile(){
         $cate_id = I('cate_id');
 		$keyword = I('keyword');
+		$map['is_show'] = 1;
 		if($cate_id != 0){
 			$map['cate_id'] = $cate_id;
 			$parameters['cate_id'] = $cate_id;
@@ -45,7 +46,7 @@ class EquipmentController extends AdminbaseController {
 		$Page->setConfig('last','末页');
 		$show = $Page->show();// 分页显示输出
 		
-		$list=D('equictive')->where($map)->limit($Page->firstRow.','.$Page->listRows)->select();
+		$list=D('equictive')->where($map)->limit($Page->firstRow.','.$Page->listRows)->order('run asc')->select();
 		foreach($list as $k=>$v){
 			$cate_name = D('equiact')->where('id=%d',array($v['cate_id']))->getField('cate_name');
 			$list[$k]['cate_name'] = $cate_name;
@@ -94,6 +95,7 @@ class EquipmentController extends AdminbaseController {
 		}
 	}
 	
+	
 	public function savemobileajax(){
 		$id = I('id');
 		$data=array(
@@ -109,7 +111,35 @@ class EquipmentController extends AdminbaseController {
 			$this->ajaxReturn(array('result'=>0));
 		}
 	}
-
+	//删除至回收站
+	public function changeshow(){
+		$id = I('id');
+		$is_show = I('is_show');
+		
+		$result=D('equictive')->where('id=%d',array($id))->setField('is_show',$is_show);
+		if($is_show == 1){
+			$msg = '还原';
+		}else{
+			$msg = '删除';
+		}
+		if($result){
+			$this->ajaxReturn(array('result'=>1,'msg'=>$msg.'成功'));
+		}else{
+			$this->ajaxReturn(array('result'=>0,'msg'=>$msg.'失败'));
+		}
+	}
+	//彻底删除
+	public function delmobile(){
+		$id = I('id');
+		$result=D('equictive')->where('id=%d',array($id))->delete();
+		
+		if($result){
+			$this->ajaxReturn(array('result'=>1,'msg'=>'删除成功'));
+		}else{
+			$this->ajaxReturn(array('result'=>0,'msg'=>'删除失败'));
+		}
+	}
+	
     public function act(){        
         $data = D('equiact')->select();
 		foreach($data as $k=>$v){
@@ -217,7 +247,50 @@ class EquipmentController extends AdminbaseController {
     		}
     	}
     }
-       
-
+      
+	public function erecycle(){
+        $cate_id = I('cate_id');
+		$keyword = I('keyword');
+		$map['is_show'] = 0;
+		if($cate_id != 0){
+			$map['cate_id'] = $cate_id;
+			$parameters['cate_id'] = $cate_id;
+		}
+		if($keyword != ''){
+			$map1['cdkey'] = array('like','%'.$keyword.'%');
+			$map1['alias'] = array('like','%'.$keyword.'%');
+			$map1['_logic'] = 'or';
+			$map['_complex'] = $map1;
+			$parameters['keyword'] = $keyword;
+		}
+		
+		$count=D('equictive')->where($map)->count();
+		$Page = new \Think\Page($count,96,$parameters);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$Page->setConfig('first','第一页');
+		$Page->setConfig('last','末页');
+		$show = $Page->show();// 分页显示输出
+		
+		$list=D('equictive')->where($map)->limit($Page->firstRow.','.$Page->listRows)->order('run asc')->select();
+		foreach($list as $k=>$v){
+			$cate_name = D('equiact')->where('id=%d',array($v['cate_id']))->getField('cate_name');
+			$list[$k]['cate_name'] = $cate_name;
+			
+			$weiximap['cdkey'] = $v['cdkey'];
+			$numb = D('weixi')->where($weiximap)->count('cdkey');
+			$list[$k]['numb'] = $numb;
+			$userinfo = $this->Getuserbyid($v['authorid']);
+			$list[$k]['username'] = $userinfo['user_login'];
+		}
+		$aryi=D('runcode')->getField('id,taskname',true);
+		
+		$equiact = D('equiact')->select();
+		
+		$this->assign('parameters',$parameters);
+		$this->assign('equiact',$equiact);
+		$this->assign('aryi',$aryi);
+        $this->assign('list',$list);
+		$this->assign('page',$show);
+        $this->display();
+    }
 }
 
