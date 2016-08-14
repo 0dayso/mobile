92b68276f58149811f06facad245a518addea4b2
zharty <zharty@163.com>
2016-08-14 15:34:56 +0800

    8-14

--- application/Admin/Controller/MobileController.class.php
+++ application/Admin/Controller/MobileController.class.php
@@ -237,7 +237,7 @@ class MobileController extends AdminbaseController{
 			$sql="SELECT id,STATUS FROM mbl_mobile GROUP BY mobile HAVING COUNT(*)>1 AND `status`=0 ORDER BY id DESC";
 			$result=M()->query($sql);
 			$ary=array();
-			$count=count($result)>200?200:count($result);			
+			$count=count($result)>200?200:count($result);
 			for($i=0;$i<$count;$i++){
 				$map['id']=$result[$i]['id'];
 				$sul=M('mobile')->where($map)->delete();
