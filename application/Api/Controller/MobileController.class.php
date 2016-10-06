fef5117e3d55b475d4a510e62b722848f0a7d124
zharty <zharty@163.com>
2016-10-06 13:06:05 +0800

    数据

--- application/Api/Controller/MobileController.class.php
+++ application/Api/Controller/MobileController.class.php
@@ -69,6 +69,7 @@ class MobileController extends Controller {
     public function getmboiletype(){       
         M()->startTrans();        
         $data=M('mobile')->field('id,mobile')->where('status=0 and type=2')->lock(true)->find();
+        var_dump($data);
         $t=M('mobile')->where("id=%d",$data['id'])->setField('isshow',1); 
         M()->commit();
         if(!$t){
