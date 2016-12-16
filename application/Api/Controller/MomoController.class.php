a7d6ba0a53c2baf1bfb6a6852972bceee9a52702
zharty <zharty@163.com>
2016-12-16 14:30:57 +0800

    数据

---
+++ application/Api/Controller/MomoController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 验证码处理
+ */
+namespace Api\Controller;
+use Think\Controller;
+class MomoController extends Controller {
+
+    public function index() {
+       echo "dsfsdfds";
+    }
+    
+
+}
+
