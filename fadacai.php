b7776243b75a635c402de9c87a13b40e95f37505
zharty <zharty@163.com>
2016-12-27 16:22:26 +0800

    1

--- fadacai.php
+++ fadacai.php
@@ -5,6 +5,9 @@
  */
 
 //header('Location:http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile');
+ header("html/text");
  echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile");
+ exit();
+
 ?>
 
