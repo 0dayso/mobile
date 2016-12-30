891d58922c948f0db2f1a33702fd815466b8f13e
zharty <zharty@163.com>
2016-12-30 16:23:16 +0800

    数据

--- application/Api/Controller/TongluyunController.class.php
+++ application/Api/Controller/TongluyunController.class.php
@@ -1,35 +1,11 @@
 <?php
-namespace Admin\Controller;
-use Common\Controller\AdminbaseController;
+namespace Api\Controller;
+use Think\Controller;
 class TongluyunController extends AdminbaseController{
-	protected $navcat_model;
-    public static $numbpage=0;
 
-	function _initialize() {
-		parent::_initialize();
-		$this->navcat_model =D("Common/NavCat");
-	}
+
 	function index(){
-		if($_POST){
-			$sul=M("option")->where("option_name='tlymsg'")->find();
-
-			if(!$sul){
-				$data['option_name']="tlymsg";
-				$data['option_value']="哥我是张丹";
-				M("option")->add($data);	
-			}else{
-				$map['option_name']="tlymsg";
-				$data['option_value']=I("request.name");
-				M("option")->where($map)->save($data);
-			}
-		}else{
-			$this->display();
-		}
-
-		
-	}
-	function addmsg(){
-		$sul=M("option")->where("option_name='tlymsg'")->getfield("option_value");
+		$sul=M("options")->where("option_name='tlymsg'")->getfield("option_value");
 		echo $sul;
 		
 	}
