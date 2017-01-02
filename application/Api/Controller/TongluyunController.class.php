52dbc049142efda186562e58d47f52a18fe8e19b
zharty <zharty@163.com>
2017-01-02 20:26:15 +0800

    13

--- application/Api/Controller/TongluyunController.class.php
+++ application/Api/Controller/TongluyunController.class.php
@@ -3,12 +3,10 @@ namespace Api\Controller;
 use Think\Controller;
 class TongluyunController  extends Controller{
 
-
 	function index(){
-		$sul=M("options")->where("option_name='tlymsg'")->find();
+		$sul=M("options")->where("option_name='tlymsg' or option_name='tlynmb'")->getfield("option_name,option_value");
 		$this->ajaxreturn($sul,'xml');
 	}
-
 }
 
 ?>
