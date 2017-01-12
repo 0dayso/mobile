d1c14c43869e0c10a27daf1ae1e389fb65a22979
zharty <zharty@163.com>
2017-01-12 15:33:02 +0800

    数据

--- book.php
+++ book.php
@@ -8,7 +8,7 @@
 
 header('Content-type:text/json');
 $row=$_REQUEST['row'];
-echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile&row=".$row);
+echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobilebook&a=phonemobile&row=".$row);
 exit();
 ?>
 
