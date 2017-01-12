45478f7942d80c71b5c6b9c19c450413b47c8ae2
zharty <zharty@163.com>
2017-01-12 15:57:24 +0800

    1

--- book.php
+++ book.php
@@ -7,8 +7,12 @@
 //header('Location:http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile');
 
 header('Content-type:text/json');
-$row=$_REQUEST['row'];
-echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobilebook&a=phonemobile&row=".$row);
+if(isset($_REQUEST['row'])){
+	$row=$_REQUEST['row'];	
+}else{
+	$row=1;
+}
+echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile&row=".$row);
 exit();
 ?>
 
