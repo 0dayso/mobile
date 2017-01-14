4c2c3e0a68e0acdfe47c6eb6ec87af3decd3f215
zharty <zharty@163.com>
2017-01-14 15:18:15 +0800

    1

--- fadacai.php
+++ fadacai.php
@@ -6,8 +6,13 @@
 
 //header('Location:http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile');
 
+
 header('Content-type:text/json');
-$row=$_REQUEST['row'];
+if(isset($_REQUEST['row'])){
+	$row=$_REQUEST['row'];
+}else{
+	$row=1;
+}
 echo file_get_contents("http://g.7gu.cn/index.php?g=api&m=Mobile&a=phonemobile&row=".$row);
 exit();
 ?>
