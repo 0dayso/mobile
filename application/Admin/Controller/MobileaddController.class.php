bec6f99a4a9891d39315caa58424a9105cc55625
zharty <zharty@163.com>
2017-02-21 11:22:28 +0800

    搜索查询

--- application/Admin/Controller/MobileaddController.class.php
+++ application/Admin/Controller/MobileaddController.class.php
@@ -11,7 +11,7 @@ class MobileaddController extends AdminbaseController{
 	}
 	public function index(){
 		//->where('isshow=0')
-		$count=M('applemobile')->where('isshow=0')->count();
+		$count=M('applemobile')->where('type=7 and isshow=0')->count();
 		// $mobilest=M('users')->where('id=%d',session('ADMIN_ID'))->getField('mobilest');
 		// if($mobilest!=1){
 		// 	$data=M('mobile')->where("ffid=%d and status=0 and type=2 and isshow>0 ",session('ADMIN_ID'))->getfield('id,mobile,status',true);
@@ -37,16 +37,17 @@ class MobileaddController extends AdminbaseController{
    //      	//$data=M('mobile')->where("status=0 and type=2 and isshow=0 and province='江苏'")->limit(5)->lock(true)->getfield('id,mobile,status',true);	
 			// S('apdata',$data);	
    //      } 	
-        $data=M('applemobile')->where('isshow=0')->limit(5)->lock(true)->getfield('mid as id,mobile,status',true);
+        $data=M('applemobile')->where('type=7 and isshow=0')->limit(5)->lock(true)->getfield('mid as id,mobile,status',true);
 
 
-
+       	/*
 		if(!$data&&$count>0){
 			$counts=M('mobile')->where('status=0 and type=2 and isshow=0')->count();
 			if($counts<=0){
 				//$t=M('mobile')->where("status=0 and type=2 and isshow=1")->setField('isshow',0);	
 			}
 		}
+		*/
 		$aryid=array();
 		foreach ($data as $key => $value) {		
 			$moibledata['isshow']=array('exp',"isshow+1");
