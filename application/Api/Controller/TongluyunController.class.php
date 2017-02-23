842c196834d9cd9581f695c82a5186a8c858313b
zharty <zharty@163.com>
2017-02-23 11:16:37 +0800

    数据

--- application/Api/Controller/TongluyunController.class.php
+++ application/Api/Controller/TongluyunController.class.php
@@ -7,6 +7,31 @@ class TongluyunController  extends Controller{
 		$sul=M("options")->where("option_name='tlymsg' or option_name='tlynmb'")->getfield("option_name,option_value");
 		$this->ajaxreturn($sul,'xml');
 	}
+
+	/**
+	微信通讯录已经增加使用
+	**/
+	function typeyi(){
+		$id=I('get.id');
+		$sex=I('get.sex');//性别
+		if(!$id){
+			$result['status']=0;
+			$result['msg']="不存在有效数据";
+		}
+		$data['type']=11;//已使用的数据
+		$data['updatetime']=time();
+		$data['sex']=$sex;
+
+		$where['mid']=$id;
+		$sul=M("applemobile")->where($where)->save($data);
+		if($sul){
+			$result['status']=1;
+			$result['msg']="修改成功";
+		}
+		$this->ajaxretrun($result);
+	}
+
+
 }
 
 ?>
