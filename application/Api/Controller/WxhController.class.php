959c3a50acfd54f1de06c1e2eb38f9e60b6e4930
zharty <zharty@163.com>
2017-06-09 13:44:42 +0800

    数据

--- application/Api/Controller/WxhController.class.php
+++ application/Api/Controller/WxhController.class.php
@@ -16,9 +16,9 @@ class WxhController extends Controller{
 		$data['status']=0;
 		M()->startTrans();
 		try{
-			$sul=M("wxid_show")->field("wid,wxid,wxpwd,wx62")->where(array('status'=>1))->find();	
+			$sul=M("wxid_show")->field("wid,wxid,wxpwd,wx62")->where(array('status'=>1))->find();//查询一个微信号
 			if($sul){
-				$e=M("wxid_show")->where('wid=%d',$sul['wid'])->setInc('status');
+				$e=M("wxid_show")->where('wid=%d',$sul['wid'])->setInc('status'); //更改显示状态加1
 				if($e){
 					$dat['status']=1;
 					M()->commit();
@@ -26,13 +26,11 @@ class WxhController extends Controller{
 				}else{
 					$dat['status']=4;	
 					M()->rollbanck();
-				}
-				
+				}				
 			}else{
 				$data['status']=3;
 				M()->rollbanck();	
-			}
-			
+			}			
 		}catch(\excption $t){	
 			M()->rollbanck();	
 			$data['status']=2;
